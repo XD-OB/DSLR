f3be169ac88ea13b404b88f66a73b317042a8ff8
Anas Elouargui <aelouarg@e1r10p15.1337.ma>
2020-12-24 18:48:05 +0100

    update hatgame

--- hatgame.py
+++ hatgame.py
@@ -25,13 +25,14 @@ harry = [
 def print_menu(stdscr, selected_row_idx):
 	stdscr.clear()
 	h, w = stdscr.getmaxyx()
+	maxlen = len(max(harry, key=len))
 	for idx, row in enumerate(harry):
-		x = 0
+		x = w//2 - maxlen//2
 		y = idx
 		stdscr.addstr(y, x, row)
 	for idx, row in enumerate(menu):
 		x = w//2 - len(row)//2
-		y = h//2 - len(menu)//2 + idx
+		y = h//2 - len(menu)//2 + idx + 9
 		if idx == selected_row_idx:
 			stdscr.attron(curses.color_pair(1))
 			stdscr.addstr(y, x, row)
